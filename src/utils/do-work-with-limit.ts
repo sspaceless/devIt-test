17bcb54f57c6e856f908aabafba8371c4f88035f
spaceless <bodnarillia26@gmail.com>
2024-02-16 20:05:34 +0200

    fix: get immediately response from each primse

--- src/utils/do-work-with-limit.ts
+++ src/utils/do-work-with-limit.ts
@@ -1,13 +1,53 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import pLimit from "p-limit";
 
-export const doWorkWithLimit = async (limit: number) => {
+import { Response } from "../store/response/types";
+
+export const doWorkWithLimit = async (
+  limit: number,
+  onResponse: (response: Response) => void,
+) => {
+  if (limit === 0) {
+    return;
+  }
+
   const limiter = pLimit(limit);
 
-  const tasks = Array(...Array(1000)).map((value, index) =>
-    limiter(() => axios.post("http://localhost:8080/api", { index })),
+  const tasks = Array(...Array(100)).map((value, index) =>
+    limiter(() =>
+      axios.post<{ index: number }>("http://localhost:8080/api", { index }),
+    ),
   );
 
-  const result = await Promise.all(tasks);
-  return result;
+  // Promise.allSettled(tasks).then((results) => console.log(results));
+
+  tasks.forEach((promise) => {
+    promise
+      .then((result) => {
+        const response = {
+          index: +result.data.index,
+          status: result.status,
+          message: result.statusText,
+        };
+        onResponse(response);
+      })
+      .catch((error) => {
+        const axiosError = error as AxiosError<{ message: string }>;
+
+        const errorStatusCode =
+          axiosError.response?.status ?? "unexpected status";
+        const errorMessage =
+          axiosError.response?.statusText ?? "something went wrong";
+
+        const response = {
+          index: undefined,
+          status: +errorStatusCode,
+          message: errorMessage,
+        };
+
+        console.log(error);
+
+        onResponse(response);
+      });
+  });
 };
