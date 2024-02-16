e37800ae5a7769b777db9b5464fc71dad9427e6a
spaceless <bodnarillia26@gmail.com>
2024-02-16 12:50:15 +0200

    feat: created ui

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import { Button, Card } from "@mui/material";
+import { useFormik } from "formik";
+import { FC, useState } from "react";
+
+import { Input } from "./components/UI";
+import { inputSchema } from "./schemas";
+import { FormValues } from "./types";
+import { doWorkWithLimit } from "./utils";
+
+const App: FC = () => {
+  const [isStarted, setIsStarted] = useState(false);
+
+  const formik = useFormik<FormValues>({
+    initialValues: { limit: 0 },
+    validationSchema: inputSchema,
+    onSubmit: async ({ limit }) => {
+      console.log("submit");
+      setIsStarted(true);
+
+      await doWorkWithLimit(limit);
+
+      setIsStarted(false);
+    },
+  });
+
+  const handleStartButtonClick = () => {
+    formik.submitForm();
+  };
+
+  return (
+    <div className="flex h-screen w-full flex-col items-center p-4">
+      <Card className="flex w-full flex-col gap-2 p-2 sm:w-1/2 sm:max-w-[600px]">
+        <Input
+          id="limit"
+          label="limit"
+          variant="outlined"
+          placeholder="enter the limit"
+          type="number"
+          InputProps={{ inputProps: { min: "0", max: "100" } }}
+          disabled={isStarted}
+          defaultValue={formik.initialValues.limit}
+          touched={formik.touched.limit}
+          errors={formik.errors.limit}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+
+        <div className="self-center">
+          <Button variant="outlined" onClick={handleStartButtonClick}>
+            start
+          </Button>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default App;
